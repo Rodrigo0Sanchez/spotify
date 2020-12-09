da2d7320c557dbd24a2d26a230e82e1a60e1bfc8
Rodrigo0Sanchez <sanchez.salinas.gianpier.rodrigo@iisgalvanimi.edu.it>
2020-12-09 12:41:22 +0000

    punto 1.2

--- first-routed-app/src/app/spotify.service.ts
+++ first-routed-app/src/app/spotify.service.ts
@@ -14,8 +14,7 @@ export class SpotifyService {
   searchTrack(query: string) {
     const url = `https://api.spotify.com/v1/search?q=${query}&type=track`;
     const headers = new HttpHeaders({
-      Authorization:
-        'Bearer BQBt9Ai0Lcd0xnczKivODSe8gWuUCCKOoWBN9ybH4_5MO3RHAZqVJVnJD_1yG3__4N9QiR3_BuNRrR45SnaujzvcDA0oDfDV-NV0xyHoMa_xo9NlM9IcXZzfb5Oeypy_GdiFQvd2nibOjTlHuvHX4546HIrL5l_s1rH5LjBZz01gjBZceVkt_Mbb_R50vvDS6svK35QZXisvQW4YSG-uGvhfqYJpUWZXdEIdfxayvuS0TyWrlvEvPW0nZeybNgkKmP7glYx_V2iyoscJG1gNXYJFWoE'
+      Authorization: environment.oauthToken
     });
 
     let obsTracks = this.http.get(url, { headers });
@@ -30,6 +29,23 @@ export class SpotifyService {
     return this.http.get(url, { headers });
   }
 
+  searchArtist(query: string) {
+    const url = `https://api.spotify.com/v1/search?q=${query}&type=artists`;
+    const headers = new HttpHeaders({
+      Authorization: environment.oauthToken
+    });
+
+    let obsArtist = this.http.get(url, { headers });
+    return obsArtist;
+ //Ritorno un observable ai componenti che richiedono il servizio
+  }
+
+  getArtist(id: string) {
+    const url = `https://api.spotify.com/v1/artists/${id}`;
+    const headers = new HttpHeaders({Authorization: environment.oauthToken});
+
+    return this.http.get(url, { headers });
+  }
 
 
 }
